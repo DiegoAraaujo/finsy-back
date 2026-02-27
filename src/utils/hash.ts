fa34a2f3dc676ef4db4fa103d485ee3e476a1ac2
DiegoAraaujo <diegoaraaujo8@gmail.com>
2026-02-27 15:24:49 -0300

    feat: Adds a function to compare password hashes.

--- src/utils/hash.ts
+++ src/utils/hash.ts
@@ -5,3 +5,10 @@ export const generateHash = async (password: string): Promise<string> => {
   const hashedPassword = await bcrypt.hash(password, saltRounds);
   return hashedPassword;
 };
+
+export const compareHash = async (
+  password: string,
+  hash: string,
+): Promise<boolean> => {
+  return await bcrypt.compare(password, hash);
+};

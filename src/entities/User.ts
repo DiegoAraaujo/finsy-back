20f08a16aa2907223fc2e028a56639aa5b7599b9
DiegoAraaujo <diegoaraaujo8@gmail.com>
2026-02-27 13:32:47 -0300

    feat: Add a User entity with basic properties and methods.

---
+++ src/entities/User.ts
@@ -0,0 +1,35 @@
+class User {
+  private id?: number;
+  private name: string;
+  private email: string;
+  private passwordHash: string;
+
+  constructor(name: string, email: string, passwordHash: string, id?: number) {
+    this.id = id;
+    this.name = name;
+    this.email = email;
+    this.passwordHash = passwordHash;
+  }
+
+  getId() {
+    return this.id;
+  }
+  getName() {
+    return this.name;
+  }
+  getEmail() {
+    return this.email;
+  }
+  getPasswordHash() {
+    return this.passwordHash;
+  }
+  toPersistence() {
+    return {
+      name: this.name,
+      email: this.email,
+      passwordHash: this.passwordHash,
+    };
+  }
+}
+
+export default User;

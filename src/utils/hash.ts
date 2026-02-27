10e842e7a66f3fb5bf94d349a52b5a4c0bf5a563
DiegoAraaujo <diegoaraaujo8@gmail.com>
2026-02-27 13:36:03 -0300

    feat: Add a function to generate password hashes using bcrypt.

---
+++ src/utils/hash.ts
@@ -0,0 +1,7 @@
+import bcrypt from "bcrypt";
+
+export const generateHash = async (password: string): Promise<string> => {
+  const saltRounds = 10;
+  const hashedPassword = await bcrypt.hash(password, saltRounds);
+  return hashedPassword;
+};

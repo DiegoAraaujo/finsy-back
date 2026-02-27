5ed4ffd9d5cedd2dc9d0f3e28fdd726dc909b925
DiegoAraaujo <diegoaraaujo8@gmail.com>
2026-02-27 14:46:37 -0300

    feat: Updates user validation to allow blank spaces and makes fields optional.

--- src/validators/userValidator.ts
+++ src/validators/userValidator.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const createUserSchema = z.object({
-  name: z.string().min(1, "name is required"),
+  name: z.string().trim().min(1, "name is required"),
   email: z.string().email("invalid email"),
   password: z
     .string()
@@ -9,9 +9,6 @@ export const createUserSchema = z.object({
 });
 
 export const updateUserSchema = z.object({
-  name: z.string().min(1),
+  name: z.string().trim().min(1).optional(),
   email: z.string().email().optional(),
-  password: z
-    .string()
-    .min(6, "The password must be at least 8 characters long."),
 });

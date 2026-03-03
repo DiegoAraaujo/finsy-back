6252baf1ef39efb00167d9056017562f1826f51a
DiegoAraaujo <diegoaraaujo8@gmail.com>
2026-03-03 10:05:24 -0300

    feat(user-router): Adds new routes and authentication middleware

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,13 +1,33 @@
 import { FastifyInstance } from "fastify";
-import { createUserController, deleteUserController } from "../modules/users";
+import {
+  createUserController,
+  deleteUserController,
+  updateUserController,
+  loginController,
+} from "../modules/users";
 import { CreateUserDTO } from "../modules/users/dtos/CreateUserDTO";
+import { UpdateUserDTO } from "../modules/users/dtos/UpdateUserDTO";
+import { LoginDTO } from "../modules/users/dtos/LoginDTO";
+import auth from "../middleware/auth";
 
 const usersRoutes = async (router: FastifyInstance) => {
   router.post<{ Body: CreateUserDTO }>("/", (req, reply) =>
     createUserController.execute(req, reply),
   );
 
-  router.delete("/", (req, reply) => deleteUserController.execute(req, reply));
+  router.post<{ Body: LoginDTO }>("/login", (req, reply) =>
+    loginController.execute(req, reply),
+  );
+
+  router.put<{ Body: UpdateUserDTO }>(
+    "/",
+    { preHandler: [auth] },
+    (req, reply) => updateUserController.execute(req, reply),
+  );
+
+  router.delete("/", { preHandler: [auth] }, (req, reply) =>
+    deleteUserController.execute(req, reply),
+  );
 };
 
 export default usersRoutes;

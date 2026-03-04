f62f33ef77df75928031a22f796efd8075e8fa24
DiegoAraaujo <diegoaraaujo8@gmail.com>
2026-03-04 09:06:46 -0300

    feat: export use cases and category controllers

---
+++ src/modules/categories/index.ts
@@ -0,0 +1,27 @@
+import CategoryRepository from "../../repository/CategoryRepository";
+
+import CreateCategoryUseCase from "./use-cases/CreateCategoryUseCase";
+import CreateCategoryController from "./controllers/CreateCategoryController";
+
+import DeleteCategoryUseCase from "./use-cases/DeleteCategoryUseCase";
+import DeleteCategoryController from "./controllers/DeleteCategoryController";
+
+import UpdateCategoryUseCase from "./use-cases/UpdateCategoryUseCase";
+import UpdateCategoryController from "./controllers/UpdateCategoryController";
+
+const categoryRepository = new CategoryRepository();
+
+const createCategoryUseCase = new CreateCategoryUseCase(categoryRepository);
+export const createCategoryController = new CreateCategoryController(
+  createCategoryUseCase,
+);
+
+const deleteCategoryUseCase = new DeleteCategoryUseCase(categoryRepository);
+export const deleteCategoryController = new DeleteCategoryController(
+  deleteCategoryUseCase,
+);
+
+const updateCategoryUseCase = new UpdateCategoryUseCase(categoryRepository);
+export const updateCategoryController = new UpdateCategoryController(
+  updateCategoryUseCase,
+);

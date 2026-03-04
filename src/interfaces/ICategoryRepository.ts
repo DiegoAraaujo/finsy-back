62e87a397d8e79d7ddadde4f7f024b1f028c142a
DiegoAraaujo <diegoaraaujo8@gmail.com>
2026-03-04 09:00:56 -0300

    feat(ICategoryRepository): Interface for the category repository

---
+++ src/interfaces/ICategoryRepository.ts
@@ -0,0 +1,12 @@
+import Category from "../entities/Category";
+
+export interface ICategoryRepository {
+  createCategory(category: Category): Promise<Category>;
+  deleteCategory(categoryId: number): Promise<void>;
+  updateCategory(
+    categoryId: number,
+    updates: { name?: string; spendingLimit?: number },
+  ): Promise<Category>;
+  findCategoryByName(name: string, monthId: number): Promise<Category | null>;
+  findCategoryById(categoryId: number): Promise<Category | null>;
+}

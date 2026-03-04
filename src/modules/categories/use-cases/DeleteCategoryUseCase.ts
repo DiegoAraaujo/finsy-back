dac755293bd4efaacc8c33d9edecdae03e6efca9
DiegoAraaujo <diegoaraaujo8@gmail.com>
2026-03-04 09:05:59 -0300

    feat: Add controller and use case to delete category.

---
+++ src/modules/categories/use-cases/DeleteCategoryUseCase.ts
@@ -0,0 +1,26 @@
+import { ICategoryRepository } from "../../../interfaces/ICategoryRepository";
+import UseCaseError from "../../../interfaces/UseCaseError";
+
+class DeleteCategoryUseCase {
+  private categoryRepository: ICategoryRepository;
+
+  constructor(categoryRepository: ICategoryRepository) {
+    this.categoryRepository = categoryRepository;
+  }
+
+  async execute(categoryId: number) {
+    const existingCategory =
+      await this.categoryRepository.findCategoryById(categoryId);
+
+    if (!existingCategory) {
+      throw <UseCaseError>{
+        message: "the category does not exist",
+        errorType: "CATEGORY_DOES_NOT_EXISTS",
+      };
+    }
+
+    return await this.categoryRepository.deleteCategory(categoryId);
+  }
+}
+
+export default DeleteCategoryUseCase;

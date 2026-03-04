d45a9515cb5896ce6a1ccf1aab59984f1db2de8a
DiegoAraaujo <diegoaraaujo8@gmail.com>
2026-03-04 09:00:18 -0300

    feat: attribute for month ID, verification for the ID, and method for data persistence

--- src/entities/Category.ts
+++ src/entities/Category.ts
@@ -1,27 +1,42 @@
 class Category {
   private id?: number;
-  private userId: number;
+  private monthId: number;
   private name: string;
   private spendingLimit: number;
 
-  constructor(userId: number, id: number, name: string, spendingLimit: number) {
+  constructor(
+    monthId: number,
+    name: string,
+    spendingLimit: number,
+    id?: number,
+  ) {
     this.id = id;
-    this.userId = userId;
+    this.monthId = monthId;
     this.name = name;
     this.spendingLimit = spendingLimit;
   }
 
   getId() {
+    if (!this.id) {
+      throw new Error("Category ID is not defined");
+    }
     return this.id;
   }
-  getUserId() {
-    return this.userId;
+  getMonthId() {
+    return this.monthId;
   }
   getName() {
     return this.name;
   }
   getSpendingLimit() {
     return this.spendingLimit;
+  }
+  toPersistence() {
+    return {
+      name: this.name,
+      spendingLimit: this.spendingLimit,
+      monthId: this.monthId,
+    };
   }
 }
 

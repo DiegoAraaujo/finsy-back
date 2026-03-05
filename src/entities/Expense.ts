08a6cd7f148fee8d9f16921f62eb8e527118d9fe
DiegoAraaujo <diegoaraaujo8@gmail.com>
2026-03-05 17:10:25 -0300

    feat: adds the entity that represents expenses

--- src/entities/Expense.ts
+++ src/entities/Expense.ts
@@ -1,46 +1,35 @@
-import { PaymentMethod } from "@prisma/client";
-
 class Expense {
   private id?: number;
-  private userId: number;
+  private monthId: number;
   private categoryId: number;
-  private snapshotId: number;
   private amount: number;
   private description: string;
-  private paymentMethod: PaymentMethod;
-  private effectiveFrom: Date;
 
   constructor(
-    userId: number,
+    monthId: number,
     categoryId: number,
-    snapshotId: number,
     amount: number,
     description: string,
-    paymentMethod: PaymentMethod,
-    effectiveFrom: Date,
     id?: number,
   ) {
-    this.id = id;
-    this.userId = userId;
     this.categoryId = categoryId;
-    this.snapshotId = snapshotId;
+    this.id = id;
+    this.monthId = monthId;
     this.amount = amount;
     this.description = description;
-    this.paymentMethod = paymentMethod;
-    this.effectiveFrom = effectiveFrom;
   }
 
   getId() {
+    if (!this.id) {
+      throw new Error("Category ID is not defined");
+    }
     return this.id;
   }
-  getUserId() {
-    return this.userId;
+  getMonthId() {
+    return this.monthId;
   }
   getCategoryId() {
     return this.categoryId;
-  }
-  getSnapshotId() {
-    return this.snapshotId;
   }
   getAmount() {
     return this.amount;
@@ -48,11 +37,13 @@ class Expense {
   getDescription() {
     return this.description;
   }
-  getPaymentMethod() {
-    return this.paymentMethod;
-  }
-  getEffectiveFrom() {
-    return this.effectiveFrom;
+  toPersistence() {
+    return {
+      amount: this.amount,
+      description: this.description,
+      categoryId: this.categoryId,
+      monthId: this.monthId,
+    };
   }
 }
 

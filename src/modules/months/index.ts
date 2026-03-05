0252f8024318cad40afe73389669d85933b6f674
DiegoAraaujo <diegoaraaujo8@gmail.com>
2026-03-05 16:13:21 -0300

    feat: export the controllers for the mont

---
+++ src/modules/months/index.ts
@@ -0,0 +1,35 @@
+import MonthRepository from "../../repository/MonthRepository";
+
+import CreateMonthUseCase from "./use-cases/CreateMonthUseCase";
+import CreateMonthController from "./controllers/CreateMonthController";
+
+import GetAllMonthsUseCase from "./use-cases/GetAllMonthsUseCase";
+import GetAllMonthsController from "./controllers/GetAllMonthsController";
+
+import GetCurrentMonthUseCase from "./use-cases/GetCurrentMonthUseCase";
+import GetCurrentMonthController from "./controllers/GetCurrentMonthController";
+
+import GetLatestMonthUseCase from "./use-cases/GetLatestMonthUseCase";
+import GetLatestMonthController from "./controllers/GetLatestMonthController";
+
+const monthRepository = new MonthRepository();
+
+const createMonthUseCase = new CreateMonthUseCase(monthRepository);
+export const createMonthController = new CreateMonthController(
+  createMonthUseCase,
+);
+
+const getAllMonthsUseCase = new GetAllMonthsUseCase(monthRepository);
+export const getAllMonthsController = new GetAllMonthsController(
+  getAllMonthsUseCase,
+);
+
+const getCurrentMonthUseCase = new GetCurrentMonthUseCase(monthRepository);
+export const getCurrentMonthController = new GetCurrentMonthController(
+  getCurrentMonthUseCase,
+);
+
+const getLatestMonthUseCase = new GetLatestMonthUseCase(monthRepository);
+export const getLatestMonthController = new GetLatestMonthController(
+  getLatestMonthUseCase,
+);

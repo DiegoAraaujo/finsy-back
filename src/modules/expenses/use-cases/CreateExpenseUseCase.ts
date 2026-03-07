c40529fd52194d983c104c8f20182435302d27e9
DiegoAraaujo <diegoaraaujo8@gmail.com>
2026-03-07 08:42:35 -0300

    feat: Create a controller, use case, and DTO for creating an expense.

---
+++ src/modules/expenses/use-cases/CreateExpenseUseCase.ts
@@ -0,0 +1,32 @@
+import { PaymentMethod } from "@prisma/client";
+import Expense from "../../../entities/Expense";
+import { IExpenseRepository } from "../../../interfaces/IExpenseRepository";
+
+class CreateExpenseUseCase {
+  private expenseRepository: IExpenseRepository;
+
+  constructor(expenseRepository: IExpenseRepository) {
+    this.expenseRepository = expenseRepository;
+  }
+
+  async execute(
+    monthId: number,
+    categoryId: number,
+    amount: number,
+    paymentMethod: PaymentMethod,
+    description: string | null,
+  ) {
+    const expenseEntity = new Expense(
+      monthId,
+      categoryId,
+      amount,
+      paymentMethod,
+      description,
+    );
+
+    const expense = await this.expenseRepository.createExpense(expenseEntity);
+    return expense;
+  }
+}
+
+export default CreateExpenseUseCase;

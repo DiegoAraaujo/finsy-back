d62bf79e8dca46f0f6dcbfe0d0554d01c27fd463
DiegoAraaujo <diegoaraaujo8@gmail.com>
2026-03-09 14:33:20 -0300

    feat: exports controller that deletes expenses

--- src/modules/expenses/index.ts
+++ src/modules/expenses/index.ts
@@ -1,10 +1,17 @@
 import ExpenseRepository from "../../repository/ExpensesRepository";
 import CreateExpenseController from "./controllers/CreateExpenseController";
+import DeleteExpenseController from "./controllers/DeleteExpenseController";
 import CreateExpenseUseCase from "./use-cases/CreateExpenseUseCase";
+import DeleteExpenseUseCase from "./use-cases/DeleteExpenseUseCase";
 
 const expenseRepository = new ExpenseRepository();
 
 const createExpenseUseCase = new CreateExpenseUseCase(expenseRepository);
 export const createExpenseController = new CreateExpenseController(
   createExpenseUseCase,
+);
+
+const deleteExpenseUseCase = new DeleteExpenseUseCase(expenseRepository);
+export const deleteExpenseController = new DeleteExpenseController(
+  deleteExpenseUseCase,
 );

ddd6f3ce83c9808d692f28376e6e0fd6f637e472
DiegoAraaujo <diegoaraaujo8@gmail.com>
2026-03-11 16:12:02 -0300

    feat(app): feat: add Swagger docs, cookie support, and JWT refresh token setup

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,42 @@
 import "dotenv/config";
 import fastify from "fastify";
 import fastifyJwt from "@fastify/jwt";
+import fastifyCookie from "@fastify/cookie";
 import usersRoutes from "./routes/user";
 import categoriesRoutes from "./routes/category";
 import MonthsRoutes from "./routes/month";
 import expensesRoutes from "./routes/expense";
-
+import swagger from "@fastify/swagger";
+import swaggerUI from "@fastify/swagger-ui";
 const app = fastify();
+
+(async () => {
+  await app.register(swagger, {
+    openapi: {
+      info: {
+        title: "Finsy API",
+        description: "API para controle financeiro",
+        version: "1.0.0",
+      },
+    },
+  });
+
+  await app.register(swaggerUI, {
+    routePrefix: "/docs",
+  });
+})();
 
 app.register(fastifyJwt, {
   secret: process.env.JWT_ACCESS_SECRET as string,
+});
+
+app.register(fastifyCookie);
+
+app.register(fastifyJwt, {
+  secret: process.env.JWT_REFRESH_SECRET as string,
+  namespace: "refresh",
+  jwtVerify: "refreshJwtVerify",
+  jwtSign: "refreshJwtSign",
 });
 
 app.register(usersRoutes, { prefix: "/api/users" });

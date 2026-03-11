86e15dd7988454157385d9d11f4c00dc2bb16c32
DiegoAraaujo <diegoaraaujo8@gmail.com>
2026-03-11 07:18:19 -0300

    feat: records the remaining routes for expenses.

--- src/routes/expense.ts
+++ src/routes/expense.ts
@@ -2,16 +2,37 @@ import { FastifyInstance } from "fastify";
 import {
   createExpenseController,
   deleteExpenseController,
+  getExpensesByCategoryController,
+  getExpensesByMonthController,
+  updateExpenseController,
 } from "../modules/expenses";
 import auth from "../middleware/auth";
 import { CreateExpenseDTO } from "../modules/expenses/dtos/CreateExpenseDTO";
+import { updateExpenseDTO } from "../modules/expenses/dtos/UpdateExpenseDTO";
 
 const expensesRoutes = async (router: FastifyInstance) => {
+  router.get<{
+    Params: { monthId: string };
+  }>("/month/:monthId", { preHandler: [auth] }, (req, reply) =>
+    getExpensesByMonthController.execute(req, reply),
+  );
+
+  router.get<{
+    Params: { categoryId: string };
+  }>("/category/:categoryId", { preHandler: [auth] }, (req, reply) =>
+    getExpensesByCategoryController.execute(req, reply),
+  );
   router.post<{
     Body: CreateExpenseDTO;
     Params: { monthId: string; categoryId: string };
   }>("/:monthId/:categoryId", { preHandler: [auth] }, (req, reply) =>
     createExpenseController.execute(req, reply),
+  );
+  router.put<{
+    Body: updateExpenseDTO;
+    Params: { expenseId: string };
+  }>("/:expenseId", { preHandler: [auth] }, (req, reply) =>
+    updateExpenseController.execute(req, reply),
   );
   router.delete<{
     Params: { expenseId: string };

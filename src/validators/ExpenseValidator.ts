e50eb5188ba036d594b8c566d16ddc201368cb2d
DiegoAraaujo <diegoaraaujo8@gmail.com>
2026-03-11 07:14:32 -0300

    feat(ExpenseValidator): Implement a scheme for data updating.

--- src/validators/ExpenseValidator.ts
+++ src/validators/ExpenseValidator.ts
@@ -4,3 +4,8 @@ export const createExpenseSchema = z.object({
   amount: z.number().min(1, "Amount must be at least 1"),
   description: z.string().nullable().default(null),
 });
+
+export const updateExpenseSchema = z.object({
+  amount: z.number().min(1).optional(),
+  description: z.string().nullable().optional(),
+});

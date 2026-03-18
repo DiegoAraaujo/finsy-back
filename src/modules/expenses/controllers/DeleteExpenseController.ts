b59820e05120422cce3b41fe7950def0e4912c58
DiegoAraaujo <diegoaraaujo8@gmail.com>
2026-03-18 19:54:44 -0300

    fix(DeleteExpenseController): catch error type

--- src/modules/expenses/controllers/DeleteExpenseController.ts
+++ src/modules/expenses/controllers/DeleteExpenseController.ts
@@ -23,7 +23,16 @@ class DeleteExpenseController {
     try {
       await this.deleteExpenseUseCase.execute(expenseId);
       return reply.status(201).send({ success: true });
-    } catch (error) {
+    } catch (error: any) {
+      if ("errorType" in error) {
+        if (error.errorType === "EXPENSE_NOT_FOUND") {
+          return reply.status(400).send({
+            message: error.message,
+            details: error.details,
+          });
+        }
+      }
+
       return reply.status(500).send({ message: "internal error" });
     }
   }

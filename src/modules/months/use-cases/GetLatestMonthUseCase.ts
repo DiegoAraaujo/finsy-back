971215dd969cc899d690fde1b0ab6c18520e60f4
DiegoAraaujo <diegoaraaujo8@gmail.com>
2026-03-18 21:06:27 -0300

    feat(GetLatestMonthUseCase): Now returns to the categories of the most recent month.

--- src/modules/months/use-cases/GetLatestMonthUseCase.ts
+++ src/modules/months/use-cases/GetLatestMonthUseCase.ts
@@ -1,16 +1,38 @@
+import Category from "../../../entities/Category";
 import Month from "../../../entities/Month";
+import { ICategoryRepository } from "../../../interfaces/ICategoryRepository";
 import { IMonthRepository } from "../../../interfaces/IMonthRepository";
+import UseCaseError from "../../../interfaces/UseCaseError";
 
 class GetLatestMonthUseCase {
   private monthRepository: IMonthRepository;
+  private categoryRepository: ICategoryRepository;
 
-  constructor(monthRepository: IMonthRepository) {
+  constructor(
+    monthRepository: IMonthRepository,
+    categoryRepository: ICategoryRepository,
+  ) {
     this.monthRepository = monthRepository;
+    this.categoryRepository = categoryRepository;
   }
 
-  async execute(userId: number): Promise<Month | null> {
-    const lastestMonth = await this.monthRepository.findLatestMonth(userId);
-    return lastestMonth;
+  async execute(
+    userId: number,
+  ): Promise<{ month: Month; categories: Category[] }> {
+    const latestMonth = await this.monthRepository.findLatestMonth(userId);
+
+    if (!latestMonth) {
+      throw <UseCaseError>{
+        message: "Latest month not found for this user.",
+        errorType: "MONTH_DOES_NOT_EXISTS",
+      };
+    }
+
+    const categories = await this.categoryRepository.findCategoriesByMonthId(
+      latestMonth.getId(),
+    );
+
+    return { month: latestMonth, categories };
   }
 }
 

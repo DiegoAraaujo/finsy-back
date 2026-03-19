3bf09e5ca227e5004a92bcfb7faaa7ebb740901e
DiegoAraaujo <diegoaraaujo8@gmail.com>
2026-03-18 21:06:58 -0300

    feat(GetCurrentMonthUseCase): Now returns to the categories of the current month.

--- src/modules/months/use-cases/GetCurrentMonthUseCase.ts
+++ src/modules/months/use-cases/GetCurrentMonthUseCase.ts
@@ -1,14 +1,24 @@
+import Category from "../../../entities/Category";
 import Month from "../../../entities/Month";
+import { ICategoryRepository } from "../../../interfaces/ICategoryRepository";
 import { IMonthRepository } from "../../../interfaces/IMonthRepository";
+import UseCaseError from "../../../interfaces/UseCaseError";
 
 class GetCurrentMonthUseCase {
   private monthRepository: IMonthRepository;
+  private categoryRepository: ICategoryRepository;
 
-  constructor(monthRepository: IMonthRepository) {
+  constructor(
+    monthRepository: IMonthRepository,
+    categoryRepository: ICategoryRepository,
+  ) {
     this.monthRepository = monthRepository;
+    this.categoryRepository = categoryRepository;
   }
 
-  async execute(userId: number): Promise<Month | null> {
+  async execute(
+    userId: number,
+  ): Promise<{ month: Month; categories: Category[] }> {
     const today = new Date();
 
     const month = today.getMonth() + 1;
@@ -19,7 +29,19 @@ class GetCurrentMonthUseCase {
       month,
       year,
     );
-    return currentMonth;
+
+    if (!currentMonth) {
+      throw <UseCaseError>{
+        message: "Month does not exist.",
+        errorType: "MONTH_DOES_NOT_EXISTS",
+      };
+    }
+
+    const categories = await this.categoryRepository.findCategoriesByMonthId(
+      currentMonth.getId(),
+    );
+
+    return { month: currentMonth, categories };
   }
 }
 

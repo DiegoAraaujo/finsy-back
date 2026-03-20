86da4e683e62a386edfa7b2ed9799c5e5b488214
DiegoAraaujo <diegoaraaujo8@gmail.com>
2026-03-20 07:21:37 -0300

    feat(UpdateUserUseCase): Adds a verification that ensures the user does not update the email to an already existing one.

--- src/modules/users/use-cases/UpdateUserUseCase.ts
+++ src/modules/users/use-cases/UpdateUserUseCase.ts
@@ -14,18 +14,30 @@ class UpdateUserUseCase {
   ): Promise<User> {
     if (!updates.name && !updates.email) {
       throw <UseCaseError>{
-        message: "There is no data to update",
+        message: "No data provided to update.",
         errorType: "VALIDATION_ERROR",
       };
     }
 
-    const userExists = await this.userRepository.findById(userId);
+    const existingUser = await this.userRepository.findById(userId);
 
-    if (!userExists) {
+    if (!existingUser) {
       throw <UseCaseError>{
         message: "User not found",
         errorType: "NOT_FOUND",
       };
+    }
+    if (updates.email) {
+      const existingEmail = await this.userRepository.findByEmail(
+        updates.email,
+      );
+
+      if (existingEmail && existingEmail.getId() !== userId) {
+        throw <UseCaseError>{
+          message: "Email already registered",
+          errorType: "EMAIL_DUPLICATED",
+        };
+      }
     }
 
     const dataToUpdate: Partial<{ name?: string; email?: string }> = {};

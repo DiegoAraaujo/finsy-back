fdbaa4e13851815798bc3af16f170019a053f5b9
DiegoAraaujo <diegoaraaujo8@gmail.com>
2026-03-21 20:47:24 -0300

    feat(CreateMonthController): add MONTHLY_LIMIT_EXCEEDED handling and remove categories from response

--- src/modules/months/controllers/CreateMonthController.ts
+++ src/modules/months/controllers/CreateMonthController.ts
@@ -3,7 +3,6 @@ import CreateMonthUseCase from "../use-cases/CreateMonthUseCase";
 import { monthMapper } from "../mappers/monthMapper";
 import { CreateCategoryDTO } from "../../categories/dtos/CreateCategoryDTO";
 import { createCategoriesSchema } from "../../../validators/categoryValidator";
-import { categoryMapper } from "../../categories/mappers/CategoryMapper";
 
 class CreateMonthController {
   private createMonthUseCase: CreateMonthUseCase;
@@ -36,22 +35,23 @@ class CreateMonthController {
     }
 
     try {
-      const { month, categories } = await this.createMonthUseCase.execute(
+      const month = await this.createMonthUseCase.execute(
         userId,
         salary,
         validation.data,
       );
       return reply.status(201).send({
         month: monthMapper(month),
-        categories: categories.map((c) => categoryMapper(c)),
       });
     } catch (error: any) {
       if ("errorType" in error) {
-        if (error.errorType === "MONTH_ALREADY_EXISTS") {
-          return reply.status(400).send({
-            message: error.message,
-            details: error.details,
-          });
+        switch (error.errorType) {
+          case "MONTH_ALREADY_EXISTS":
+          case "MONTHLY_LIMIT_EXCEEDED":
+            return reply.status(400).send({
+              message: error.message,
+              details: error.details,
+            });
         }
       }
       reply.status(500).send({ message: "Internal server error" });

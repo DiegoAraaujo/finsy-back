e10a5b6c5d4e83cc35167f1f0e1953347d197370
DiegoAraaujo <diegoaraaujo8@gmail.com>
2026-04-11 07:13:25 -0300

    refactor: standardize AutoLoginController to use jwtVerify instead of refreshJwtVerify

--- src/modules/users/controllers/AutoLoginController.ts
+++ src/modules/users/controllers/AutoLoginController.ts
@@ -2,27 +2,49 @@ import { FastifyRequest, FastifyReply } from "fastify";
 import { userMapper } from "../mappers/userMapper";
 import AutoLoginUseCase from "../use-cases/AutoLoginUseCase";
 
+type JwtPayload = {
+  userId: number;
+  type: "access" | "refresh";
+};
+
 class AutoLoginController {
   private autoLoginUseCase: AutoLoginUseCase;
 
   constructor(autoLoginUseCase: AutoLoginUseCase) {
     this.autoLoginUseCase = autoLoginUseCase;
   }
+
   async execute(request: FastifyRequest, reply: FastifyReply) {
     try {
-      const { userId } = await request.refreshJwtVerify();
+      const { userId, type } = await request.jwtVerify<JwtPayload>({
+        onlyCookie: true,
+      });
+
+      if (type !== "refresh") {
+        return reply.status(401).send({ message: "Invalid token type" });
+      }
+
       const user = await this.autoLoginUseCase.execute(userId);
 
       if (!user) {
         return reply.status(404).send({ message: "User not found" });
       }
 
-      const accessToken = await reply.jwtSign({ userId }, { expiresIn: "5m" });
+      const accessToken = await reply.jwtSign(
+        { userId, type: "access" },
+        { expiresIn: "5m" }
+      );
 
-      return reply.status(200).send({ user: userMapper(user), accessToken });
+      return reply.status(200).send({
+        user: userMapper(user),
+        accessToken,
+      });
     } catch (err) {
       reply.clearCookie("finsy_refreshToken");
-      return reply.status(401).send({ message: "Not authenticated" });
+
+      return reply.status(401).send({
+        message: "Not authenticated",
+      });
     }
   }
 }
